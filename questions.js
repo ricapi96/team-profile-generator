70200dfa273c9892fef70a29732bdaf1575e1e4c
Erica Roquemore <enroquemore@gmail.com>
2021-04-21 00:00:53 -0400

    employee parent class

--- questions.js
+++ questions.js
@@ -2,7 +2,21 @@ const employeeQuestion = [
     {
         type: 'input',
         name: 'name',
-        message: 'What is the name of employee',
+        message: 'What is the name of employee?',
     },
-    
-]
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the id of employee?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the email address of employee?',
+    },
+    {
+        type: 'input',
+        name: 'role',
+        message: 'What is the role of employee?',
+    },
+];
