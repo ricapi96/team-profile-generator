bef916716d37adb1db56ad28d4d057488228b0c9
Erica Roquemore <enroquemore@gmail.com>
2021-04-21 00:15:30 -0400

    updated index.js

--- develop/index.js
+++ develop/index.js
@@ -1,3 +1,24 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
-const generateMarkdown = require("./utils/generateMarkdown.js");
+const generateHTML = require("./utils/generateHTML.js");
+ 
+
+// Function to write HTML file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log("Creating Employee Cards...")
+    );
+}
+
+// Function to initialize the app
+function init () {
+    inquirer.prompt(employeeQuestion)
+    .then((data) => {
+        writeToFile("cards.html", generateHTML(data));
+    })
+    .catch((err) => console.log(err));
+
+}
+
+// Function call to initialize the app
+init();
