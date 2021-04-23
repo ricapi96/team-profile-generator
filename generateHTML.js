f0a61361d193ba2e3f5cd73328d995f80018482d
Erica Roquemore <enroquemore@gmail.com>
2021-04-23 15:58:05 -0400

    added function to generateHTML.js

--- generateHTML.js
+++ generateHTML.js
@@ -1,9 +1,31 @@
-const createManager = (data) => {
-    
-}
+// Function to generate HTML
 
 function generateHTML(data) {
-    return `${data.employee}`
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile Generator</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="jumbotron">
+            <h1 class="text-center">My Team</h1>
+        </div>
+         <h2>${data.employeeCard}</h2>
+         <h2>${data.engineerCard}</h2> 
+         <h2>${data.managerCard}</h2>
+         <h2>${data.internCard}</h2>
+         
+    
+    
+    
+    
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    </body>
+    </html>`
 }
 
 
