28e6ab67e47760f9731d9f2b36e11e09aa4f1335
Erica Roquemore <enroquemore@gmail.com>
2021-04-23 15:08:42 -0400

    updated generateHTML.js

--- develop/index.js
+++ develop/index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
-const generateHTML = require("./src/generateHTML.js");
+const generateHTML = require("../generateHTML.js");
  
 
 // Function to write HTML file
