f280cb5b9ef6851d9bffa0992a763d479a88ae42
Erica Roquemore <enroquemore@gmail.com>
2021-04-24 00:58:26 -0400

    updated rendered html

--- generateHTML.js
+++ generateHTML.js
@@ -1,5 +1,7 @@
 // Function to generate HTML
 
+const { employeeQuestions, managerQuestions } = require("./assets/lib/questions");
+
 function generateHTML(data) {
     return `<!DOCTYPE html>
     <html lang="en">
@@ -11,7 +13,25 @@ function generateHTML(data) {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     </head>
     <body>
-    
+    <div class="jumbotron">
+		<h1 class="text-center">My Team</h1>`
+
+        `<div class="container">
+            <div class="row row-cols-3">
+              <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-header">
+                      ${employeeQuestions.employee}
+                    </div>
+                    <ul class="list-group list-group-flush">
+                      <li class="list-group-item">An item</li>
+                      <li class="list-group-item">A second item</li>
+                      <li class="list-group-item">A third item</li>
+                    </ul>
+                  </div>
+              </div>
+              </div><hr>
+              
     
  
    
@@ -23,9 +43,67 @@ function generateHTML(data) {
     </html>`
 }
 
+function renderEmployeeCard() {
+    if (employee === manager) {
+        return `<div class="container">
+        <div class="row row-cols-3">
+          <div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="card-header">
+                  ${managerQuestions, name}
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">id: ${managerQuestions, id}</li>
+                  <li class="list-group-item">email: ${managerQuestions, email}</li>
+                  <li class="list-group-item">Office #: ${managerQuestions, number}</li>
+                </ul>
+              </div>
+          </div>
+          </div><hr>`
+    
+    } else if (employee === engineer) {
+        return `<div class="container">
+        <div class="row row-cols-3">
+          <div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="card-header">
+                  ${engineerQuestions, name}
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">id: ${engineerQuestions, id}</li>
+                  <li class="list-group-item">email: ${engineerQuestions, email}</li>
+                  <li class="list-group-item">Office #: ${engineerQuestions, github}</li>
+                </ul>
+              </div>
+          </div>
+          </div><hr>`
+    } else if (employee === intern) {
+        return `<div class="container">
+        <div class="row row-cols-3">
+          <div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="card-header">
+                  ${internQuestions, name}
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">id: ${internQuestions, id}</li>
+                  <li class="list-group-item">email: ${internQuestions, email}</li>
+                  <li class="list-group-item">Office #: ${internQuestions, school}</li>
+                </ul>
+              </div>
+          </div>
+          </div><hr>`
+    }
 
 
 
+function renderEngineerCard() {}
+
+
+function renderManagerCard() {}
+
+
+function renderInternCard() {}
 
 
 
