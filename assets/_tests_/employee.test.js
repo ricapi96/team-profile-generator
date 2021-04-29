f5309a07d46ed291837c25b1f58ace91a308b3a1
Erica Roquemore <enroquemore@gmail.com>
2021-04-29 00:32:54 -0400

    updated employee.test.js

---
+++ assets/_tests_/employee.test.js
@@ -0,0 +1,32 @@
+const Employee = require("../lib/employee");
+
+describe("employee", () => {
+    it("Returns the name of the employee", () => {
+        const firstName = "Michael";
+        const novice = new Employee(officeName, 1, "mscott@dm.com");
+        expect(novice.getName()).toEqual(firstName); 
+    });
+});
+
+describe("employee", () => {
+    it("Returns the role of the employee", () => {
+        const novice = new Employee("Michael", 1, "mscott@dm.com");
+        expect(novice.getRole()).toEqual("employee");
+    });
+});
+
+describe("employee", () => {
+    it("Returns the id of the employee", () => {
+        const employId = 1;
+        const novice = new Employee("Michael", employId, "mscott@dm.com");
+        expect(novice.getId()).toEqual(employId);
+    });
+});
+
+describe("employee", () => {
+    it("Returns the email of the employee", () => {
+        const employEmail = "mscott@dm.com";
+        const novice = new Employee("Michael", 1, employEmail);
+        expect(novice.getEmail()).toEqual(employEmail);
+    });
+});
