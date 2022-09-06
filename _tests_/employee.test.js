cb1ae296eccb95d960244d63f931472719457762
Erica Roquemore <enroquemore@gmail.com>
2022-09-06 00:14:41 -0400

    employee test passed

--- _tests_/employee.test.js
+++ _tests_/employee.test.js
@@ -3,21 +3,22 @@ const Employee = require("../lib/employee");
 describe("employee", () => {
     it("Returns the name of the employee", () => {
         const firstName = "Michael";
-        const novice = new Employee(officeName, 001, "mscott@dm.com");
+        const novice = new Employee(firstName, 1, "mscott@dm.com");
         expect(novice.getName()).toEqual(firstName); 
     });
 });
 
 describe("employee", () => {
     it("Returns the role of the employee", () => {
-        const novice = new Employee("Michael", 001, "mscott@dm.com");
-        expect(novice.getRole()).toEqual("employee");
+        const testValue = "employee";
+        const novice = new Employee("Michael", 1, "mscott@dm.com");
+        expect(novice.getRole()).toBe(testValue);
     });
 });
 
 describe("employee", () => {
     it("Returns the id of the employee", () => {
-        const employId = 001;
+        const employId = 1;
         const novice = new Employee("Michael", employId, "mscott@dm.com");
         expect(novice.getId()).toEqual(employId);
     });
@@ -26,7 +27,7 @@ describe("employee", () => {
 describe("employee", () => {
     it("Returns the email of the employee", () => {
         const employEmail = "mscott@dm.com";
-        const novice = new Employee("Michael", 001, employEmail);
+        const novice = new Employee("Michael", 1, employEmail);
         expect(novice.getEmail()).toEqual(employEmail);
     });
 });
